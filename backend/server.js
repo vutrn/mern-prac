45aca7f0ba3afc7947022218526ec155dbbb487c
vutrn <meoiuwai@gmail.com>
2025-01-26 22:00:28 +0700

    refactor backend structure; update package.json to use index.js, adjust server port, and modify static file serving path

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 import express from "express";
+import path from "path";
 import { db } from "./config/db.js";
 import productRouter from "./routes/product.route.js";
-import path from "path";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json()); // to parse the incoming request with JSON payloads
@@ -14,10 +14,10 @@ app.use("/api/products", productRouter);
 
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  })
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+  });
 }
 
 app.listen(PORT, () => {
